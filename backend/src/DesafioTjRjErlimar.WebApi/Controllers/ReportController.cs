684ad591bd7f8185528a191eb73af91f0da7e402
Erlimar Silva Campos <erlimar@gmail.com>
2025-03-04 04:20:40 -0300

    Implementa versão básica do relatório de livros

--- backend/src/DesafioTjRjErlimar.WebApi/Controllers/ReportController.cs
+++ backend/src/DesafioTjRjErlimar.WebApi/Controllers/ReportController.cs
@@ -1,5 +1,6 @@
 using DesafioTjRjErlimar.Application.ManutencaoAssunto;
 using DesafioTjRjErlimar.Application.ManutencaoAutor;
+using DesafioTjRjErlimar.Application.ManutencaoLivro;
 
 using FastReport.Export.PdfSimple;
 
@@ -13,17 +14,21 @@ public class ReportController : ControllerBase
 {
     private readonly ManutencaoAutorAppService _manutencaoAutorAppService;
     private readonly ManutencaoAssuntoAppService _manutencaoAssuntoAppService;
+    private readonly ManutencaoLivroAppService _manutencaoLivroAppService;
     private readonly TimeProvider _timeProvider;
 
     public ReportController(
         ManutencaoAutorAppService manutencaoAutorAppService,
         ManutencaoAssuntoAppService manutencaoAssuntoAppService,
+        ManutencaoLivroAppService manutencaoLivroAppService,
         TimeProvider timeProvider)
     {
         _manutencaoAutorAppService = manutencaoAutorAppService
             ?? throw new ArgumentNullException(nameof(manutencaoAutorAppService));
         _manutencaoAssuntoAppService = manutencaoAssuntoAppService
             ?? throw new ArgumentNullException(nameof(manutencaoAssuntoAppService));
+        _manutencaoLivroAppService = manutencaoLivroAppService
+            ?? throw new ArgumentNullException(nameof(ManutencaoLivroAppService));
         _timeProvider = timeProvider ?? throw new ArgumentNullException(nameof(timeProvider));
     }
 
@@ -77,6 +82,33 @@ public class ReportController : ControllerBase
         ms.Seek(0, SeekOrigin.Begin);
 
         var nomeArquivo = $"relatorio-assuntos_{_timeProvider.GetUtcNow():yyyy-MM-ddTHH-mm-ss}.pdf";
+
+        return File(ms, "application/odf", nomeArquivo, enableRangeProcessing);
+    }
+
+    [HttpGet]
+    [Route("livros")]
+    public async Task<ActionResult> GerarRelatorioLivros()
+    {
+        var reportPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Reports", "CadastroLivros.frx");
+        var reportData = await _manutencaoLivroAppService.ObterLivrosAsync();
+        var report = new FastReport.Report();
+
+        report.Report.Load(reportPath);
+        report.Dictionary.RegisterBusinessObject(reportData.ToList(), "ReportDataSurce", 10, true);
+        report.Prepare();
+
+        var pdfExport = new PDFSimpleExport();
+
+        MemoryStream ms = new();
+
+        pdfExport.Export(report, ms);
+
+        var enableRangeProcessing = true;
+
+        ms.Seek(0, SeekOrigin.Begin);
+
+        var nomeArquivo = $"relatorio-livros_{_timeProvider.GetUtcNow():yyyy-MM-ddTHH-mm-ss}.pdf";
 
         return File(ms, "application/odf", nomeArquivo, enableRangeProcessing);
     }
